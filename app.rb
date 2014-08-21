37762b1c977dfe046dc358bde27e8a2b8bb6468e
Roman Pushkin <roman.pushkin@gmail.com>
2014-08-21 20:13:50 +0400

    Add cleaned version

---
+++ app.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'sinatra'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
