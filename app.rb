52da1d913c7675c253ba8406e1b81ab428f6e754
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-16 19:19:27 +0400

    Add sinatra reloader

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
