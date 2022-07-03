676e62a51fd3f8133c904880492b81bdbc6f595b
maksym-nenashev <nenashev@yahoo.com>
2022-07-04 01:24:56 +0200

    blog "poluczaetsa"

--- app.rb
+++ app.rb
@@ -14,3 +14,22 @@ get "/about" do
 get "/visit" do
     erb :visit  #podkluczenie fila HTML
   end
+
+get "/contacts" do
+    erb :contacts  #podkluczenie fila HTML
+  end
+
+post "/" do
+    @name = params[:aaa]  #Peredacza i cztenie parametra s HTML "Fail <layout.erb>"
+    @phone = params[:bbb]
+    @adres = params[:ccc]
+
+    @titel = "Thanks!"                         #Ispolzowanie peremennych s fila "message"
+    @message = ""#"Thanks #{@name}! Your phone:#{@phone}, Your adres:#{@adres}."
+     
+    f = File.open "./public/users.txt","a+"       #Otkrytie fila i sozdanie fila "./public/"
+    f.write "User:#{@name}, Phone:#{@phone}, Your Adres:#{@adres}.\n"
+    f.close
+    erb :message    
+  
+    end
