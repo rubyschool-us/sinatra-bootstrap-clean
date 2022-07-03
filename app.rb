3c511e1150a770dba371fcb3e617ffeecdd4bfe8
maksym-nenashev <nenashev@yahoo.com>
2022-07-03 21:03:33 +0200

    ismenen "about"

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+get "/" do
+    erb :  #podkluczenie fila HTML
+  end
