2ef4b68e514225e265f68315dd077510535f01dc
maksym-nenashev <nenashev@yahoo.com>
2022-07-16 21:51:21 +0200

    pony "@"

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pony'
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -30,17 +31,32 @@ post "/" do
     @adres = params[:ccc]
     @barber = params[:barber]
     @color = params[:color]
+ #//////////////////////////////////////////////////////////////////////////////////////////////
+    # hesh dla prowerki # хеш для валидации параметров
+    hh = {:aaa => "Введите ваше имя",:bbb => "Введите ваш телефон",:ccc => "Введите вашу дату"}
 
+    hh.each do  |key,value| 
+       if params[key] == "" #esli parametr pust peremennoj "@error" priswoit value iz hesha
+          @error = hh[key]
+          return erb :visit
+     end
+   end                      #2 wariant
+   #@error = hh.select {|key,_| params[key] == ''}.values.join(", ")
+   #if @error != ''
+   # return erb :index
+   #end
+#///////////////////////////////////////////////////////////////////////////////////////
     @titel = "Thanks!"                         #Ispolzowanie peremennych s fila "message"
     @message = "Thanks #{@name}! Your phone:#{@phone}, Your adres:#{@adres}, 
     Your barber:#{@barber},Your color:#{@color}."
                #./public/
-    f = File.open "./users.txt","a+"    #Otkrytie fila i sozdanie fila "./public/"
+    f = File.open "./public/users.txt","a+"    #Otkrytie fila i sozdanie fila "./public/"
     f.write "User:#{@name},Phone:#{@phone},Your Adres:#{@adres},Your barber:#{@barber},Your color:#{@color}\n"
     f.close
-    erb :message    
+     
+      erb :message
+  end    
   
-    end
     # Добавить зону /admin где по паролю будет выдаваться список тех, кто записался (из users.txt)
     
     post "/admin" do
@@ -57,4 +73,7 @@ post "/" do
         erb :admin
       end
     end
-
+   
+   post "/" do
+    
+    end
